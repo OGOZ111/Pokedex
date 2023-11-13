f80d33f3ce5c9254497f1b011d3c9dc0dfe3f7af
Luke B <142310217+OGOZ111@users.noreply.github.com>
2023-11-13 19:06:31 +0200

    Add files via upload
    cleaned up code, input params.

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
 const input = document.querySelector("#search");
 const bgens = document.querySelectorAll(".bgen");
+const grid = document.querySelectorAll(".grid-container");
+const changeText = document.querySelector("#change-text");
+
+// Gen buttons
+const press1 = document.querySelector("#gen1");
+const press2 = document.querySelector("#gen2");
+const press3 = document.querySelector("#gen3");
+const press4 = document.querySelector("#gen4");
+const press5 = document.querySelector("#gen5");
+const press6 = document.querySelector("#gen6");
+const press7 = document.querySelector("#gen7");
+const press8 = document.querySelector("#gen8");
+const press9 = document.querySelector("#gen9");
 
 let pokeData = [];
 let pokemon = [];
@@ -25,12 +38,8 @@ const generations = [
   { id: 5, limit: 72, offset: 649 },
   { id: 6, limit: 88, offset: 721 },
   { id: 7, limit: 96, offset: 809 },
-  { id: 8, limit: 116, offset: 905 },
+  { id: 8, limit: 112, offset: 905 },
 ];
-
-// for buttons use foreach to figure out which button was clicked (see speedgame for that)
-// first define generations and connect with the correct button
-// when you triger the fetchdata function, you pass the limit and offset to be correct ones
 
 const fetchData = async (limit, offset) => {
   await fetch(
@@ -99,3 +108,45 @@ const searchFiltered = () => {
 
 searchFiltered();
 fetchData();
+
+// Event listeners. Way too much code but does the job
+
+press1.addEventListener("click", function () {
+  changeText.textContent = "There are 151 Pokemon in Generation 1!";
+});
+
+press2.addEventListener("click", function () {
+  changeText.textContent = "There are 100 Pokemon in Generation 2!";
+});
+
+press3.addEventListener("click", function () {
+  changeText.textContent = "There are 135 Pokemon in Generation 3!";
+});
+
+press4.addEventListener("click", function () {
+  changeText.textContent = "There are 107 Pokemon in Generation 4!";
+});
+
+press5.addEventListener("click", function () {
+  changeText.textContent = "There are 156 Pokemon in Generation 5!";
+});
+
+press6.addEventListener("click", function () {
+  changeText.textContent = "There are 72 Pokemon in Generation 6!";
+});
+
+press7.addEventListener("click", function () {
+  changeText.textContent = "There are 88 Pokemon in Generation 7!";
+});
+
+press8.addEventListener("click", function () {
+  changeText.textContent = "There are 96 Pokemon in Generation 8!";
+});
+
+press9.addEventListener("click", function () {
+  changeText.textContent = "There are 112 Pokemon in Generation 9!";
+});
+
+input.addEventListener("keyup", function () {
+  changeText.textContent = "";
+});
